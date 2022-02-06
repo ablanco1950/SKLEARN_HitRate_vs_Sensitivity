2bbad36164e840809086af6670e665ab8d201f6d
Alfonso Blanco <alfonsoblanco5050@gmail.com>
2022-02-06 19:37:48 +0100

    Add files via upload

--- SUSY_sklearn_with_test_out_train.py
+++ SUSY_sklearn_with_test_out_train.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
-
-
+import time
+Inicio1=time.time()
+Fin1=time.time()
+Fin=Fin1-Inicio1
 from sklearn.model_selection import train_test_split
 
 #https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
@@ -9,15 +11,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import GradientBoostingClassifier
-
+#from sklearn.linear_model import LogisticRegressionClassifier
+#https://towardsdatascience.com/logistic-regression-using-python-sklearn-numpy-mnist-handwriting-recognition-matplotlib-a6b31e2b166a
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 arr=[]
 arry=[]
 
-f=open("C:\SUSY20.txt","r")
-ContaMax=400000;
+f=open("C:\SUSY.csv","r")
+ContDesde=4500000
+#ContDesde=0
+ContaMax=50000000
 Conta=0;
 for linea in f:
     Conta=Conta+1
+    if Conta < ContDesde: continue
     if Conta > ContaMax :break
     lineadelTrain =linea.split(",")
   
@@ -42,10 +50,11 @@ for linea in f:
 
 X_test=np.array(arr)
 #   print(x)
-Y_test=np.array(arry)
+Y_test_arr=np.array(arry)
+#Y_test_arr=np.array(Y_test)
 
 f=open("C:\SUSY.csv","r")
-ContaMax=300000;
+ContaMax=4500000;
 Conta=0;
 for linea in f:
     Conta=Conta+1
@@ -71,26 +80,22 @@ for linea in f:
        arry.append(1.0)
    
 
-x=np.array(arr)
-#   print(x)
-y=np.array(arry)
- 
-df = pd.DataFrame(x)
-df['Y'] = y
+Fin1=time.time()
+Fin=Fin1-Inicio1
+print ("Time in seconds spent in passing from file in disk to array in memory = " + str(Fin))
+###################################################3
+# Naive Bayes Classifier
+#################################################
 
-# Split into training and test set
-train, test = train_test_split(df, test_size = 0.2,random_state=42)
-X_train, Y_train = train.iloc[:,:-1], train.iloc[:,-1]
-# X_test, Y_test = test.iloc[:,:-1], test.iloc[:,-1]
-n_train, n_test = len(X_train), len(X_test)
-# print(X_train)   
-Y_predict, pred_test = [np.zeros(n_train), np.zeros(n_test)]
+X_train=np.array(arr)
+#   print(x)
+Y_train=np.array(arry)
 
 lm= GaussianNB()
 lm.fit(X_train,Y_train)   
 Y_predict=lm.predict(X_test)
 
-Y_test_arr=np.array(Y_test)
+
 
 TotAciertos=0.0
 TotFallos=0.0
@@ -108,9 +113,14 @@ print("")
 print("RESULTS NAIVE BAYES")     
 print("Total hits TEST = " + str(TotAciertos))
 print("Total failures TEST = " + str(TotFallos))
+
+Fin2=time.time()
+Fin=Fin2-Fin1
+print ("Time in seconds spent in Naive Bayes = " + str(Fin))
 ###################################################3
 # RandomForestClassifier
 #################################################
+
 rf= RandomForestClassifier()
 rf.fit(X_train,Y_train)   
 Y_predict=rf.predict(X_test)
@@ -127,9 +137,14 @@ for i in range (len(Y_predict)):
     else:
         TotFallos =TotFallos + 1
 print("")  
-print("RESULTS GRADIENT FOREST")    
+print("RESULTS RANDOM FOREST")    
 print("Total hits TEST = " + str(TotAciertos))
 print("Total failures TEST = " + str(TotFallos))
+
+Fin3=time.time()
+Fin=Fin3-Fin2
+print ("Time in seconds spent in Random Forest = " + str(Fin))
+
 ###################################################3
 # AdaBoostClassifier
 #################################################
@@ -153,6 +168,9 @@ print("")
 print("RESULTS ADABOOST")    
 print("Total Hits TEST = " + str(TotAciertos))
 print("Total failures TEST = " + str(TotFallos))
+Fin4=time.time()
+Fin=Fin4-Fin3
+print ("Time in seconds spent in Adaboost Classifier = " + str(Fin))
 ###################################################3
 # GradientBoostClassifier
 #################################################
@@ -173,3 +191,52 @@ print("")
 print("RESULTS GRADIENT BOOST")    
 print("Total Hits TEST = " + str(TotAciertos))
 print("Total Failuress TEST = " + str(TotFallos))
+Fin5=time.time()
+Fin=Fin5-Fin4
+print ("Time in seconds spent in Gradient Boost = " + str(Fin))
+###################################################3
+# LogisticRegressionClassifier
+#################################################
+lg=LogisticRegression()
+lg.fit(X_train,Y_train)   
+Y_predict=lg.predict(X_test)
+
+TotAciertos=0.0
+TotFallos=0.0
+  
+for i in range (len(Y_predict)):
+   
+    if (Y_predict[i]==Y_test_arr[i]):
+        TotAciertos=TotAciertos+1
+    else:
+        TotFallos =TotFallos + 1
+print("")  
+print("RESULTS LOGISTIC REGRESSION")    
+print("Total Hits TEST = " + str(TotAciertos))
+print("Total Failuress TEST = " + str(TotFallos))
+Fin6=time.time()
+Fin=Fin6-Fin5
+print ("Time in seconds spent in Logistic Regression = " + str(Fin))
+###################################################3
+# DecisionTreeClassifier
+#################################################
+dt=DecisionTreeClassifier()
+dt.fit(X_train,Y_train)   
+Y_predict=dt.predict(X_test)
+
+TotAciertos=0.0
+TotFallos=0.0
+  
+for i in range (len(Y_predict)):
+   
+    if (Y_predict[i]==Y_test_arr[i]):
+        TotAciertos=TotAciertos+1
+    else:
+        TotFallos =TotFallos + 1
+print("")  
+print("RESULTS DECISION TREE")    
+print("Total Hits TEST = " + str(TotAciertos))
+print("Total Failuress TEST = " + str(TotFallos))
+Fin7=time.time()
+Fin=Fin7-Fin6
+print ("Time in seconds spent in Decision Tree = " + str(Fin))
